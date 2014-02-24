4cb5c4434a036477a7ed8b843746e84e5886b83a
timelyportfolio <kent.russell@timelyportfolio.com>
2014-02-24 17:25:17 -0600

    get figure 1.1 working almost

--- dimple_versions/figure1_01.R
+++ dimple_versions/figure1_01.R
@@ -1,13 +1,81 @@
 #Figure 1.1
 #http://www.amazon.com/Lattice-Multivariate-Data-Visualization-Use/dp/0387759689/ref=cm_cr_pr_product_top
-
-#missing % of total rather than count
+#http://lmdvr.r-forge.r-project.org/figures/figures.html
 
 require(rCharts)
+require(lattice)
+require(plyr)
+require(dplyr)
 
 data(Chem97, package = "mlmRev")
 #not necessary but include since in example
 xtabs( ~ score, data = Chem97)
+
+lattice_plot <- histogram(~ gcsescore | factor(score), data = Chem97)
+
+#get bins like a histogram for use with dplyr
+bins <- pretty(Chem97$gcsescore,n=20)
+
+plotdata <- ddply(
+  Chem97 %.%
+    mutate(gcsescore = bins[findInterval(
+      gcsescore,
+      bins)
+    ]) %.%
+    group_by(score,gcsescore) %.%  
+    dplyr::summarise(count = length(gcsescore))
+  ,.(score,gcsescore)
+  ,summarise
+  ,count = sum(count,na.rm=F),
+  .drop = F
+)
+
+plotdata <- do.call(rbind,lapply(
+  sort(unique(Chem97$score)),
+  function(x){
+    temp <- hist(
+      (Chem97 %.% filter( score ==x ) %.% select(gcsescore))[,"gcsescore"],
+      breaks = bins,
+      plot=FALSE
+    )
+    return(data.frame(
+      score = rep(x,length(temp$mids)),
+      gcsescore = temp$breaks[-1],
+      count = temp$density
+    ))
+  }
+))
+
+
+
+d1_1 <- dPlot(
+  count ~ gcsescore,
+  data = plotdata,
+  groups = c("score"), #,"gcsescore"),
+  type = "bar",
+  height = 700,
+  width = 800
+)
+d1_1$params$facet = list( x = "score" )
+#d1_1$xAxis(
+#  type = "addMeasureAxis"
+#)
+d1_1$yAxis(
+  overrideMin = 0,
+  overrideMax = max(plotdata$count),
+  outputFormat = ".2%"
+)
+d1_1$templates$script = "./chartFacet_d3grid.html"
+d1_1
+
+#specify nrow and ncol explicitly
+d1_1$params$facet <- modifyList(
+  d1_1$params$facet,
+  list(nrow = 2, ncol=3)
+)
+d1_1
+
+
 
 chart1_1 <- rPlot(
   x = "bin(gcsescore,0.5)",
